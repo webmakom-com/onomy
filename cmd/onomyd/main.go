1f05a1a0d7ede794b9590828d2dbbfd851350165
Developer Experience team at Tendermint <hello@tendermint.com>
2021-04-15 22:29:11 -0500

    Initialized with Starport

---
+++ cmd/onomyd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	"github.com/onomyprotocol/onomy/app"
+	"github.com/onomyprotocol/onomy/cmd/onomyd/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+		os.Exit(1)
+	}
+}
