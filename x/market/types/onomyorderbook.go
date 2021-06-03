0b45f5defa017d4415db8dc33ec188f840034b43
Klim Sidorov <skn.goodwin@gmail.com>
2021-06-03 18:34:33 +0300

    Implement InsertOrderAt()

--- x/market/types/onomyorderbook.go
+++ x/market/types/onomyorderbook.go
@@ -1,6 +1,14 @@
 package types
 
 func (book OrderBook) InsertOrderAt(i int, order Order) OrderBook {
+	if len(book.Orders) == i {
+		book.Orders = append(book.Orders, &order)
+		return book
+	}
+
+	book.Orders = append(book.Orders[:i+1], book.Orders[i:]...)
+	book.Orders[i] = &order
+
 	return book
 }
 
